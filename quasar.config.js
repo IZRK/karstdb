d98dfb9a66833bff4b933d2878490ee687ab331c
jeancaffou <jean@briskula.si>
2023-10-09 09:38:14 +0200

    fix gh pages deployment path

--- quasar.config.js
+++ quasar.config.js
@@ -55,12 +55,13 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      publicPath: 'karstdb',
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
